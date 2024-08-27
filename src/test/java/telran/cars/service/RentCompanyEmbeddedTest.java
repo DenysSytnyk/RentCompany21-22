a24bdcf63342f3083730b8a5631fda96c494127e
Denys Sytnyk <denis.sytnyk@gmail.com>
2024-08-27 11:00:09 +0300

    Add test file

---
+++ src/test/java/telran/cars/service/RentCompanyEmbeddedTest.java
@@ -0,0 +1,14 @@
+package telran.cars.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class RentCompanyEmbeddedTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
